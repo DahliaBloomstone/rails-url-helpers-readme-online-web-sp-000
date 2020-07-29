729fe84758dbacc642d1cbb28cfac6f86802c115
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-29 11:23:32 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show]
 end
+
+#This will create routing methods for posts that we can utilize in our views and controllers. Running rails routes in the terminal will give the following output:
